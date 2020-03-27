e828cbd6f47a955725e4e2a3cb31c85035c492e0
kulvirvirk <kvirkdev@gmail.com>
2020-03-27 06:09:27 +0000

    created a add_fun that adds numbers with itself(doubles the number)

---
+++ main.py
@@ -0,0 +1,8 @@
+# 1. create a map function that return a num + num
+# 2. provide a list of number and send it to map function to double every number. 
+
+
+# 1. create a map function that return a num + num
+def add_fun(num):
+  return num + num
+
